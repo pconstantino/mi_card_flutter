befe5efddce480f56927edd9ef6d4acc1e79126d
Pedro Constantino <pedro.constantino@ctw.bmwgroup.com>
2019-10-23 09:49:08 +0100

    Mi Card implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,84 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("images/photo.jpg"),
+              ),
+              Text(
+                "Pedro Constantino",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Pacifico"),
+              ),
+              Text(
+                "SOFTWARE DEVELOPER",
+                style: TextStyle(
+                    color: Colors.teal[100],
+                    fontSize: 20,
+                    fontFamily: "SourceSans",
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                child: Divider(
+                  color: Colors.teal[200],
+                ),
+                width: 150,
+                height: 20,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    "+351 123 456 789",
+                    style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: "SourceSans",
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    "pedro.constantino@mail.com",
+                    style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: "SourceSans",
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
